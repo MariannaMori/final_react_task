0cabd04d9d04dc75cc9bde304f6d08d0eb16ad23
Marianna Mori <marianna.mori1@gmail.com>
2021-01-12 22:49:56 +0200

    cleaning app

--- finaltask/src/App.js
+++ finaltask/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 import NaviBar from './Components/Navibar';
 import Footer from './Components/Footer';
 
