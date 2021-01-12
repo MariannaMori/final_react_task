123424be67237b1f24e5095fcadd938252d48435
Marianna Mori <marianna.mori1@gmail.com>
2021-01-12 20:32:44 +0200

    commit

--- finaltask/src/Home.js
+++ finaltask/src/Home.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Slider from './Components/Slider';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import boat from '../boat.jpg';
+
 
 export const Home = () => (
+    <div>
    <Slider />
+   <Container>
+       <Row>
+           <Col>
+             <Card style={{width: '18rem'}}>
+               <Card.Img variant="top" src={{boat}}/>
+
+            </Card>
+           </Col>
+       </Row>
+
+   </Container>
+
+   </div>
 )
