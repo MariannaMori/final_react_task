f9dc0f58f699a8b75aa9d836044f4b192685970f
Marianna Mori <marianna.mori1@gmail.com>
2021-01-13 15:39:13 +0200

    changed images in slider

--- finaltask/src/Components/Main/BlogCard.js
+++ finaltask/src/Components/Main/BlogCard.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const BlogCard = ({ title, author, desc, img, link }) => {
   return (
     <div className="BlogCard">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <em>{author}</em>
       <p>{desc}</p>
       <img src={img} alt={title} />
